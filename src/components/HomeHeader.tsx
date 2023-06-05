5efebead31e313583d8457db80d25fe2a85e4e6b
matheusrocha1406 <mr140603@gmail.com>
2023-06-05 16:47:34 -0300

    Header do home estilizada

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,17 +1,29 @@
-import { Heading, HStack, Text, VStack } from "native-base";
+import { Heading, HStack, Text, VStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
 
 export function HomeHeader() {
-    return (
-        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
-            <VStack>
-            <Text color="gray.100" fontSize="md">
-                Olá, 
-            </Text>
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/matheusr1406.png" }}
+        size={16}
+        alt="Imagem do user"
+        mr={4}
+      />
 
-            <Heading color="white" fontSize="md">
-                Matheus
-            </Heading>
-            </VStack>
-        </HStack>
-    )
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Matheus
+        </Heading>
+      </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
 }
