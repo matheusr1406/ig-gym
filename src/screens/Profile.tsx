aa04cfccf8bb89a28e5ca3d79d06938ed5a95350
matheusrocha1406 <mr140603@gmail.com>
2023-06-06 15:47:02 -0300

    add userphoto with skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,12 +1,31 @@
-import { Center, Text, VStack } from "native-base";
-
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function Profile() {
-    return (
-        <VStack flex={1} bg="gray.700">
-        <Center my={24}>
-            <Text color="white">Profile</Text>
+    const [photoisloading, setPhotoIsLoading] = useState(false);
+  return (
+    <VStack flex={1} bg="gray.700">
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          { photoisloading ? 
+            <Skeleton
+            h={33}
+            w={33}
+            rounded="full"
+            startColor="gray.400"
+            endColor="gray.300"
+          />
+          :
+          <UserPhoto
+            source={{ uri: "https://github.com/matheusr1406.png" }}
+            alt="foto perfil"
+            size={33}
+          />}
         </Center>
-        </VStack>
-    )
+      </ScrollView>
+    </VStack>
+  );
 }
