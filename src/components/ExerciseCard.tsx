e409165d82f978dfb3671e5568b4d1a08c0e6913
matheusrocha1406 <mr140603@gmail.com>
2023-06-06 10:36:56 -0300

    create cards

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,34 @@
+import { Heading, HStack, Image, Text, VStack, Icon } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import {Entypo} from '@expo/vector-icons'
+
+type Props = TouchableOpacityProps & {
+
+}
+
+export function ExerciseCard({...rest}: Props) {
+    return (
+        <TouchableOpacity {...rest}>
+            <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
+                <Image 
+                source={{uri: "https://alcateiademonstros.com.br/wp-content/uploads/2022/01/remada-unilateral-com-halter.jpg"}}
+                alt="imagem treino"
+                w={16}
+                h={16}
+                rounded="md"
+                mr={4}
+                resizeMode="center"
+                />
+                <VStack flex={1}>
+                    <Heading fontSize="lg" color="white">
+                        Remada unilateral
+                    </Heading>
+                    <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
+                        3 séries x 12 reps
+                    </Text>
+                </VStack>
+                <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+            </HStack>
+        </TouchableOpacity>
+    )
+}
